4f3dd0f5d0a1396ec16df29fd485431cdf15d4e4
Hans Larsen <hans@larsen.online>
2024-01-19 13:37:27 -0800

    Add CONTRIBUTING.md and a few frontend dev fixes and features (#3)

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -3,7 +3,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-        target: 'http://127.0.0.1:8000/',
+        target: process.env.REACT_APP_API_URL ?? 'http://127.0.0.1:8000/',
         changeOrigin: true,
     })
   );
